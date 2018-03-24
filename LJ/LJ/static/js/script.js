8cbaa4b25a6204510c7ad3f4a23485ed18b21a61
Ashwin Sankar <ashwinoscar@gmail.com>
2018-03-23 18:22:24 -0600

    no trans: need to work

--- LJ/LJ/static/js/script.js
+++ LJ/LJ/static/js/script.js
@@ -1,3 +1,3 @@
-$(document).ready(function() {
-  d3.select("body").append("h1").html("Hello, World!")
-});
+// $(document).ready(function() {
+//   d3.select("body").append("h1").html("Hello, World!")
+// });
